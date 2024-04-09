a5a01a2ad419ebb5df4fb1d3341de349644ca75b
Kenan Kartal <contact@kenankartal.name>
2024-04-09 15:32:11 +0300

    Add common files

---
+++ inc/linux-64.s
@@ -0,0 +1,18 @@
+# Common Linux Definitions
+
+# System Call Numbers
+.equ SYS_EXIT, 60
+.equ SYS_READ, 0
+.equ SYS_WRITE, 1
+.equ SYS_OPEN, 2
+.equ SYS_CLOSE, 3
+.equ SYS_BRK, 12
+
+# Standard File Descriptors
+.equ STDIN, 0
+.equ STDOUT, 1
+.equ STDERR, 2
+
+# Common Status Codes
+.equ EOF, 0
+
